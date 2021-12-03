9183e5add3e9fca8e7043b4c68fcafc268918da5
Shah Aafak <85013327+Shahaafak3@users.noreply.github.com>
2021-12-03 19:32:33 +0300

    Update search_video.py

--- core/search_video.py
+++ core/search_video.py
@@ -7,13 +7,13 @@ from core.get_cookies import (
     get_cookies,
     set_cookies
 )
-from core.login import pdisk_login
+from core.login import doodstream_login
 
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36"
 search_api = "https://doodapi.com/api/search/videos?key=-1&item_id=&title={}&pageSize=10&pageNo=1&desc=&status=&sortField=ctime&sortAsc=0&needDirName=true"
 
 
-async def search_pdisk_videos(query: str, username: str, password: str) -> Union[dict, Exception]:
+async def search_doodstream_videos(query: str, username: str, password: str) -> Union[dict, Exception]:
     try:
         cookies = await get_cookies(username, password)
         response = requests.get(search_api.format(parse.quote(query)), cookies={"Cookie": cookies}, headers={"User-Agent": user_agent})
